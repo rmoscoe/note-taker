f3fa578d53cc7a1d6e577f21deb5f3cb86f234de
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-01-28 20:06:24 -0800

    add helper function to generate note ID

---
+++ helpers/noteid.js
@@ -0,0 +1,14 @@
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const usedIds = [];
+
+function generateId () {
+    let newId = "";
+    for (let i = 0; i < 8; i++) {
+        newId += chars[Math.floor(Math.random() * chars.length)];
+    }
+    if (usedIds.includes(newId)) {
+        return generateId();
+    }
+    usedIds.push(newId);
+    return newId;
+}
---
+++ routes/notes.js
@@ -0,0 +1,7 @@
+const notes = require("express").Router();
+const fs = require("fs");
+const noteid = require("../helpers/noteid");
+
+
+
+module.exports = notes;
